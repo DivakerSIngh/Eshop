ee1199143dedc5c4fcb49d46fa1ba3f85302afd2
Amit Chauhan <amitrajput12@gmail.com>
2018-04-12 22:24:37 +0530

    amit

--- ConfirmOrder.aspx.cs
+++ ConfirmOrder.aspx.cs
@@ -15,22 +15,22 @@ public partial class ConfirmOrder : System.Web.UI.Page
     DB obj;
     protected void Page_Load(object sender, EventArgs e)
     {
-    
-            if (Session["loginid"] != null && Session["loginid"].ToString() != "")
+
+        if (Session["loginid"] != null && Session["loginid"].ToString() != "")
+        {
+            if (!IsPostBack)
             {
-                if (!IsPostBack)
-                {
                 string[] usersid = Session["loginid"].ToString().Split(',');
                 load_cartlist(usersid[0]);
                 rbtnAddress.SelectedIndex = 0;
-                }
-                
             }
-            else
-            {
-                Server.Transfer("Login.aspx");
-            }
-        
+
+        }
+        else
+        {
+            Server.Transfer("Login.aspx");
+        }
+
     }
 
     private void load_cartlist(string userid)
@@ -42,18 +42,18 @@ public partial class ConfirmOrder : System.Web.UI.Page
             rptCartProds.DataSource = ds.Tables[0];
             rptCartProds.DataBind();
 
-                 DataSet dss = obj.GetCartInfoQtyNdAmt(userid);
-                 if (dss.Tables.Count > 0 && dss.Tables[0].Rows.Count > 0)
-                 {
-                   lblQuantity.Text =  dss.Tables[0].Rows[0]["qty"].ToString();
-                   lblCartNum.Text = "My Cart(" + dss.Tables[0].Rows[0]["qty"].ToString() + ")";
-                   lblPriceAmt.Text = dss.Tables[0].Rows[0]["totalamount"].ToString();
-                  // pnlMsg.Visible = false;
-                 }
+            DataSet dss = obj.GetCartInfoQtyNdAmt(userid);
+            if (dss.Tables.Count > 0 && dss.Tables[0].Rows.Count > 0)
+            {
+                lblQuantity.Text = dss.Tables[0].Rows[0]["qty"].ToString();
+                lblCartNum.Text = "My Cart(" + dss.Tables[0].Rows[0]["qty"].ToString() + ")";
+                lblPriceAmt.Text = dss.Tables[0].Rows[0]["totalamount"].ToString();
+                // pnlMsg.Visible = false;
+            }
             else
-                 {
-                    // pnlMsg.Visible = true;
-                 }
+            {
+                // pnlMsg.Visible = true;
+            }
 
             //lblCartNum.Text = "My Cart(" + ds.Tables[0].Rows.Count.ToString() + 
             //lblQuantity.Text = ds.Tables[0].Rows.Count.ToString();
@@ -63,7 +63,7 @@ public partial class ConfirmOrder : System.Web.UI.Page
             //    amt += Convert.ToDecimal(ds.Tables[0].Rows[i]["sellingprice"].ToString());
             //}
             //lblPriceAmt.Text = amt.ToString();
-                 lblTotAmt.Text = (Convert.ToDecimal(lblPriceAmt.Text) + Convert.ToDecimal(ds.Tables[0].Rows[0]["delivery_amount"].ToString()) - Convert.ToDecimal(ds.Tables[0].Rows[0]["coupon_amt"].ToString())).ToString();
+            lblTotAmt.Text = (Convert.ToDecimal(lblPriceAmt.Text) + Convert.ToDecimal(ds.Tables[0].Rows[0]["delivery_amount"].ToString()) - Convert.ToDecimal(ds.Tables[0].Rows[0]["coupon_amt"].ToString())).ToString();
             lblDeliveryCharge.Text = ds.Tables[0].Rows[0]["delivery_amount"].ToString();
             if (ds.Tables[0].Rows[0]["coupon_amt"].ToString() == "0.00")
             {
@@ -86,7 +86,7 @@ public partial class ConfirmOrder : System.Web.UI.Page
         }
 
     }
-   
+
 
     protected void btnPayment_Command(object sender, CommandEventArgs e)
     {
@@ -110,7 +110,7 @@ public partial class ConfirmOrder : System.Web.UI.Page
                 }
 
                 string tid = obj.TransactionConfirmation(str[0], null, grandtotamt.ToString(), "Buycod", cartid, "S", null, null, qty);
-                string name = "", phone = "", pincode = "", state = "", address = "", landmark = "",city="";
+                string name = "", phone = "", pincode = "", state = "", address = "", landmark = "", city = "";
                 string[] add = dss.Tables[0].Rows[0]["Address"].ToString().Split('?');
                 name = add[0];
                 phone = add[1];
@@ -125,23 +125,23 @@ public partial class ConfirmOrder : System.Web.UI.Page
                 if (tid != "")
                 {
                     //send mail and msg
-                    string msg = "",msgretailer="";
-                    msg = obj.createEmailBodyforConfirmOrder(tid, billingaddress, grandtotamt.ToString(), dscart, dscart.Tables[0].Rows[0]["Delivery_Amount"].ToString(), dscart.Tables[0].Rows[0]["coupon_amt"].ToString(),"COD");
+                    string msg = "", msgretailer = "";
+                    msg = obj.createEmailBodyforConfirmOrder(tid, billingaddress, grandtotamt.ToString(), dscart, dscart.Tables[0].Rows[0]["Delivery_Amount"].ToString(), dscart.Tables[0].Rows[0]["coupon_amt"].ToString(), "COD");
                     obj.SendEmail(dscart.Tables[0].Rows[0]["EmailId"].ToString(), msg, "Order Confirmation Mail");
 
-                    
+
                     //send mail nd msg to logistic and retailer
-                    string[] cart=cartid.Split(',');
+                    string[] cart = cartid.Split(',');
                     string[] lidid = lid.Split(',');
                     for (int k = 0; k < cart.Length && cart[k] != ""; k++)
                     {
                         DataSet dslog = obj.GetLogisticEmailndMobileInfo(lidid[k]);
 
                         DataSet dsret = obj.GetReatilerEmailndMobileInfo(cart[k]);
-                        msgretailer = obj.createEmailBodyforRetailerndLogistic(tid, dsret.Tables[0].Rows[0]["raddress"].ToString(), dsret.Tables[0].Rows[0]["rname"].ToString(), dsret.Tables[0].Rows[0]["quantity"].ToString(), dsret.Tables[0].Rows[0]["headertitle"].ToString(), dsret.Tables[0].Rows[0]["mobile"].ToString(), dsret.Tables[0].Rows[0]["prodid"].ToString(), dsret.Tables[0].Rows[0]["org_email"].ToString(), dsret.Tables[0].Rows[0]["totalamount"].ToString(), dsret.Tables[0].Rows[0]["city"].ToString(), dsret.Tables[0].Rows[0]["landmark"].ToString(), dsret.Tables[0].Rows[0]["pincode"].ToString(), dsret.Tables[0].Rows[0]["rstate"].ToString(),billingaddress);
+                        msgretailer = obj.createEmailBodyforRetailerndLogistic(tid, dsret.Tables[0].Rows[0]["raddress"].ToString(), dsret.Tables[0].Rows[0]["rname"].ToString(), dsret.Tables[0].Rows[0]["quantity"].ToString(), dsret.Tables[0].Rows[0]["headertitle"].ToString(), dsret.Tables[0].Rows[0]["mobile"].ToString(), dsret.Tables[0].Rows[0]["prodid"].ToString(), dsret.Tables[0].Rows[0]["org_email"].ToString(), dsret.Tables[0].Rows[0]["totalamount"].ToString(), dsret.Tables[0].Rows[0]["city"].ToString(), dsret.Tables[0].Rows[0]["landmark"].ToString(), dsret.Tables[0].Rows[0]["pincode"].ToString(), dsret.Tables[0].Rows[0]["rstate"].ToString(), billingaddress);
                         obj.SendEmail(dsret.Tables[0].Rows[0]["org_email"].ToString(), msgretailer, "Order Confirmation Mail");
 
-                        
+
                         string sms = "One Product has been sold form your store.";
                         if (dslog.Tables.Count > 0 && dslog.Tables[0].Rows.Count > 0)
                         {
@@ -149,7 +149,7 @@ public partial class ConfirmOrder : System.Web.UI.Page
                             obj.SendEmail(dslog.Tables[0].Rows[0]["LEmailAddress1"].ToString(), msgretailer, "Order Confirmation Mail");
                         }
                     }
-                    
+
 
                     //ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "Toast Message", "toastr.success('Order Successfully placed !');", true);
                     //redirect some where
@@ -163,88 +163,90 @@ public partial class ConfirmOrder : System.Web.UI.Page
             }
             else
                 if (rbtnAddress.SelectedValue == "W")
+            {
+                DataSet ds = obj.GetUserTypeAndBalance(str[0]);//user wallet balance
+                DataSet dss = obj.GetCartInfoQtyNdAmt(str[0]);//total amount to be paid
+                decimal grandtotamt = Convert.ToDecimal(dss.Tables[0].Rows[0]["totalamount"].ToString()) - Convert.ToDecimal(dss.Tables[0].Rows[0]["Delivery_Amount"].ToString()) - Convert.ToDecimal(dss.Tables[0].Rows[0]["Coupon_Amt"].ToString());
+                //find updated balance and prod quantity
+                if (grandtotamt <= Convert.ToDecimal(ds.Tables[0].Rows[0]["Balance"].ToString()))
                 {
-                    DataSet ds = obj.GetUserTypeAndBalance(str[0]);//user wallet balance
-                    DataSet dss = obj.GetCartInfoQtyNdAmt(str[0]);//total amount to be paid
-                    decimal grandtotamt = Convert.ToDecimal(dss.Tables[0].Rows[0]["totalamount"].ToString()) - Convert.ToDecimal(dss.Tables[0].Rows[0]["Delivery_Amount"].ToString()) - Convert.ToDecimal(dss.Tables[0].Rows[0]["Coupon_Amt"].ToString());
-                    //find updated balance and prod quantity
-                    if (grandtotamt <= Convert.ToDecimal(ds.Tables[0].Rows[0]["Balance"].ToString()))
+                    decimal bal = Convert.ToDecimal(ds.Tables[0].Rows[0]["Balance"].ToString()) - grandtotamt;
+                    DataSet dscart = obj.GetAllCartIdandQty(str[0]);
+                    string cartid = "", qty = "", lid = "";
+                    for (int i = 0; i < dscart.Tables[0].Rows.Count; i++)
                     {
-                        decimal bal = Convert.ToDecimal(ds.Tables[0].Rows[0]["Balance"].ToString()) - grandtotamt;
-                        DataSet dscart = obj.GetAllCartIdandQty(str[0]);
-                        string cartid = "", qty = "";
-                        for (int i = 0; i < dscart.Tables[0].Rows.Count; i++)
+                        cartid += dscart.Tables[0].Rows[i]["cartid"].ToString() + ",";
+                        qty += dscart.Tables[0].Rows[i]["quantity"].ToString() + ",";
+                        lid += dscart.Tables[0].Rows[i]["lid"].ToString() + ",";
+                    }
+
+                    string tid = obj.TransactionConfirmation(str[0], null, grandtotamt.ToString(), "Buywallet", cartid, "S", bal.ToString(), null, qty);
+
+                    string name = "", phone = "", pincode = "", state = "", address = "", landmark = "", city = "";
+                    string[] add = dss.Tables[0].Rows[0]["Address"].ToString().Split('?');
+                    name = add[0];
+                    phone = add[1];
+                    address = add[2];
+                    city = add[3];
+                    state = add[4];
+                    pincode = add[5];
+                    landmark = add[6];
+
+                    string billingaddress = name + "," + landmark + "," + address + "," + city + "," + state + "," + pincode + "\n" + "Phone : " + phone;
+
+
+                    if (tid != "")
+                    {
+                        //send mail nd msg
+                        string msg = "", msgretailer = "";
+                        msg = obj.createEmailBodyforConfirmOrder(tid, billingaddress, grandtotamt.ToString(), dscart, dscart.Tables[0].Rows[0]["Delivery_Amount"].ToString(), dscart.Tables[0].Rows[0]["coupon_amt"].ToString(), "COD");
+                        obj.SendEmail(dscart.Tables[0].Rows[0]["EmailId"].ToString(), msg, "Order Confirmation Mail");
+
+                        
+                        //send mail nd msg to logistic and retailer
+                        string[] cart = cartid.Split(',');
+                        string[] logisticId = lid.Split(',');
+                        for (int k = 0; k < cart.Length; k++)
                         {
-                            cartid += dscart.Tables[0].Rows[i]["cartid"].ToString() + ",";
-                            qty += dscart.Tables[0].Rows[i]["quantity"].ToString() + ",";
-                        }
-
-                        string tid = obj.TransactionConfirmation(str[0], null, grandtotamt.ToString(), "Buywallet",cartid,"S",bal.ToString(), null,qty);
-
-                        string name = "", phone = "", pincode = "", state = "", address = "", landmark = "", city = "";
-                        string[] add = dss.Tables[0].Rows[0]["Address"].ToString().Split('?');
-                        name = add[0];
-                        phone = add[1];
-                        address = add[2];
-                        city = add[3];
-                        state = add[4];
-                        pincode = add[5];
-                        landmark = add[6];
-
-                        string billingaddress = name + "," + landmark + "," + address + "," + city + "," + state + "," + pincode + "\n" + "Phone : " + phone;
+                            DataSet dslog = obj.GetLogisticEmailndMobileInfo(logisticId[k]);
+                            DataSet dsret = obj.GetReatilerEmailndMobileInfo(cart[k]);
+                            msgretailer = obj.createEmailBodyforRetailerndLogistic(tid, dsret.Tables[0].Rows[0]["raddress"].ToString(), dsret.Tables[0].Rows[0]["rname"].ToString(), dsret.Tables[0].Rows[0]["quantity"].ToString(), dsret.Tables[0].Rows[0]["headertitle"].ToString(), dsret.Tables[0].Rows[0]["mobile"].ToString(), dsret.Tables[0].Rows[0]["prodid"].ToString(), dsret.Tables[0].Rows[0]["org_email"].ToString(), dsret.Tables[0].Rows[0]["totalamount"].ToString(), dsret.Tables[0].Rows[0]["city"].ToString(), dsret.Tables[0].Rows[0]["landmark"].ToString(), dsret.Tables[0].Rows[0]["pincode"].ToString(), dsret.Tables[0].Rows[0]["rstate"].ToString(), billingaddress);
+                            obj.SendEmail(dsret.Tables[0].Rows[0]["org_email"].ToString(), msgretailer, "Order Confirmation Mail");
 
 
-                        if(tid != "")
-                        {
-                            //send mail nd msg
-                            string msg = "", msgretailer="";
-                            msg = obj.createEmailBodyforConfirmOrder(tid, billingaddress, grandtotamt.ToString(), dscart, dscart.Tables[0].Rows[0]["Delivery_Amount"].ToString(), dscart.Tables[0].Rows[0]["coupon_amt"].ToString());
-                            obj.SendEmail(dscart.Tables[0].Rows[0]["EmailId"].ToString(), msg, "Order Confirmation Mail");
-
-                            DataSet dslog = obj.GetLogisticEmailndMobileInfo();
-                            //send mail nd msg to logistic and retailer
-                            string[] cart = cartid.Split(',');
-                            for (int k = 0; k < cart.Length; k++)
+                            string sms = "One Product has been sold form your store.";
+                            if (dslog.Tables.Count > 0 && dslog.Tables[0].Rows.Count > 0)
                             {
-                                DataSet dsret = obj.GetReatilerEmailndMobileInfo(cart[k]);
-                                msgretailer = obj.createEmailBodyforRetailerndLogistic(tid, dsret.Tables[0].Rows[0]["raddress"].ToString(), dsret.Tables[0].Rows[0]["rname"].ToString(), dsret.Tables[0].Rows[0]["quantity"].ToString(), dsret.Tables[0].Rows[0]["headertitle"].ToString(), dsret.Tables[0].Rows[0]["mobile"].ToString(), dsret.Tables[0].Rows[0]["prodid"].ToString(), dsret.Tables[0].Rows[0]["org_email"].ToString(), dsret.Tables[0].Rows[0]["totalamount"].ToString(), dsret.Tables[0].Rows[0]["city"].ToString(), dsret.Tables[0].Rows[0]["landmark"].ToString(), dsret.Tables[0].Rows[0]["pincode"].ToString(), dsret.Tables[0].Rows[0]["rstate"].ToString(),billingaddress);
-                                obj.SendEmail(dsret.Tables[0].Rows[0]["org_email"].ToString(), msgretailer, "Order Confirmation Mail");
-
-
-                                string sms = "One Product has been sold form your store.";
-                                if (dslog.Tables.Count > 0 && dslog.Tables[0].Rows.Count > 0)
-                                {
-                                    obj.SendMessage(dslog.Tables[0].Rows[0]["mobile"].ToString(), sms);
-                                    obj.SendEmail(dslog.Tables[0].Rows[0]["LEmailAddress1"].ToString(), msgretailer, "Order Confirmation Mail");
-                                }
+                                obj.SendMessage(dslog.Tables[0].Rows[0]["mobile"].ToString(), sms);
+                                obj.SendEmail(dslog.Tables[0].Rows[0]["LEmailAddress1"].ToString(), msgretailer, "Order Confirmation Mail");
                             }
-                    
-
-                            //ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "Toast Message", "toastr.success('Order Successfully placed !');", true);
-                            //redirect some where
-                            Response.Redirect("MyOrder.aspx");
-                        }
-                        else
-                        {
-                            ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "Toast Message", "toastr.error('Server error cannot place order try later !');", true);
                         }
 
 
+                        //ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "Toast Message", "toastr.success('Order Successfully placed !');", true);
+                        //redirect some where
+                        Response.Redirect("MyOrder.aspx");
                     }
                     else
                     {
-                        ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "Toast Message", "toastr.error('Insufficient balance in Wallet cannot place order !');", true);
+                        ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "Toast Message", "toastr.error('Server error cannot place order try later !');", true);
                     }
+
 
                 }
                 else
-                    if (rbtnAddress.SelectedValue == "N")
-                    {
+                {
+                    ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "Toast Message", "toastr.error('Insufficient balance in Wallet cannot place order !');", true);
+                }
+
+            }
+            else if (rbtnAddress.SelectedValue == "N")
+            {
 
 
-                    }
+            }
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
 
         }
@@ -255,20 +257,18 @@ public partial class ConfirmOrder : System.Web.UI.Page
     }
     protected void rbtnAddress_SelectedIndexChanged(object sender, EventArgs e)
     {
-        if(rbtnAddress.SelectedIndex == 0)
+        if (rbtnAddress.SelectedIndex == 0)
         {
 
         }
-        else
-            if(rbtnAddress.SelectedIndex == 1)
+        else if (rbtnAddress.SelectedIndex == 1)
         {
 
         }
-        else
-                if(rbtnAddress.SelectedIndex == 2)
-                {
+        else if (rbtnAddress.SelectedIndex == 2)
+        {
 
-                }
+        }
     }
 
 
@@ -293,11 +293,11 @@ public partial class ConfirmOrder : System.Web.UI.Page
                 //amt = Convert.ToDecimal(sp.ToString()) * Convert.ToDecimal(qty.ToString());
                 //totsp.Text = amt.ToString();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
 
             }
         }
     }
-    
+
 }
